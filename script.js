92b553b1400d90a2e445e5c883c39881b56cdda2
ibrahim <ibrahimmuhieddine02@gmail.com>
2023-04-06 18:45:31 -0700

    validated the length and character usage

--- script.js
+++ script.js
@@ -12,7 +12,20 @@ function writePassword() {
   var Uppercase = confirm("Should your password have Uppercase letters")
   var Numbers = confirm("Should your password include numbers")
   var Specialcharacters = confirm("Should your password have special characters")
+
+  // validate
+  if (length < 8 || length > 128) {
+    alert("Password length must be between 8 and 128 characters.");
+    return;
+  }
+
+  if (! includeLowercase && !Uppercase && !Numbers && !Specialcharacters){
+    alert("At least one character type must be selected.");
+    return;
+  }
   
+ 
+
 
 
   passwordText.value = password;
